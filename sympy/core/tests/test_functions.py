c60b1fce735e172970cb658dacad3a72e533a794
Brian Granger <ellisonbg@gmail.com>
2011-06-24 19:08:22 -0700

    Initial tests for functional derivatives added and passing.

--- sympy/core/tests/test_functions.py
+++ sympy/core/tests/test_functions.py
@@ -371,3 +371,28 @@ def test_issue2300():
                 noraise = eq.diff(*v)
             else:
                 raises(ValueError, 'eq.diff(*v)')
+
+def test_functional_deriv():
+    t = Symbol('t')
+    xfunc = Function('x')
+    yfunc = Function('y')
+    x = xfunc(t)
+    xd = diff(x, t)
+    xdd = diff(xd, t)
+    y = yfunc(t)
+    yd = diff(y, t)
+    ydd = diff(yd, t)
+    assert diff(x, t) == xd
+    assert diff(2 * x + 4, t) == 2 * xd
+    assert diff(2 * x + 4 + y, t) == 2 * xd + yd
+    assert diff(2 * x + 4 + y * x, t) == 2 * xd + x * yd + xd * y
+    assert diff(2 * x + 4 + y * x, x) == 2 + y
+    assert (diff(4 * x**2 + 3 * x + x * y, t) == 3 * xd + x * yd + xd * y +
+            8 * x * xd)
+    assert (diff(4 * x**2 + 3 * xd + x * y, t) ==  3 * xdd + x * yd + xd * y +
+            8 * x * xd)
+    assert diff(4 * x**2 + 3 * xd + x * y, xd) == 3
+    assert diff(4 * x**2 + 3 * xd + x * y, xdd) == 0
+    assert diff(sin(x), t) == xd * cos(x)
+    assert diff(exp(x), t) == xd * exp(x)
+    assert diff(sqrt(x), t) == xd / (2 * sqrt(x))
